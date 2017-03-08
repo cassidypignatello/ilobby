23f5c10071f237f0fb422b2c40dae7354b3dc97b
cassidypignatello <cassidy.pignatello@gmail.com>
2017-03-08 16:59:26 -0500

    installs simple-router and moves initial screen into Home component

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,43 +1,16 @@
 import React, { Component, PropTypes } from 'react'
 import { StyleSheet, View } from 'react-native'
-import { Text } from 'react-native-elements'
-import LoginForm from '../components/LoginForm'
-import HomeButtons from '../components/HomeButtons'
-import TabBar from '../components/TabBar'
+import Router from 'react-native-simple-router'
+import Home from '../components/Home'
 
 export default class iLobby extends Component {
   render() {
     return (
-      <View style={styles.container}>
-          <Text h1 style={styles.welcome}>
-            iLobby
-          </Text>
-          <Text h4 style={styles.title}>
-            Crowdfunding Legislation
-          </Text>
-          <LoginForm/>
-          <HomeButtons/>  
-        <TabBar/>
-      </View>
+      <Home />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    // alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    // padding: 10
-  },
-  welcome: {
-    textAlign: 'center',
-    margin: 20
-  },
-  title: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  }
+  
 });
