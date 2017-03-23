c0078641b3b031e4414b47bdcfc43631cdbabdd6
cassidypignatello <cassidy.pignatello@gmail.com>
2017-03-23 17:56:35 -0400

    adds basic list of initial bills and placeholder images

--- app/components/Bills.js
+++ app/components/Bills.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react'
 import { StyleSheet, View } from 'react-native'
-import { Text } from 'react-native-elements'
+import { List, ListItem } from 'react-native-elements'
+
+const list = [
+  {
+    name: 'Colorado Senate Bill 113',
+    avatar_url: 'https://unsplash.it/128/?random',
+    subtitle: 'Repeal Large Magazine Ammo Ban'
+  },
+  {
+    name: 'New York Senate Bill S1747',
+    avatar_url: 'https://unsplash.it/128/?random',
+    subtitle: 'Marijuana Regulation and Taxation Act'
+  },
+  {
+    name: 'North Carolina House Bill 2',
+    avatar_url: 'https://unsplash.it/128/?random',
+    subtitle: 'Public Facilities Privacy & Security Act'
+  }
+]
 
 export default class Bills extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Bills are displayed here!</Text>
-      </View>
+      <List containerStyle={styles.container}>
+        {
+          list.map((l, i) => (
+            <ListItem
+              roundAvatar
+              avatar={{uri:l.avatar_url}}
+              key={i}
+              title={l.name}
+              subtitle={l.subtitle}
+            />
+          ))
+        }
+      </List>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    marginTop: 63, 
+    marginBottom: 20
   }
 });
